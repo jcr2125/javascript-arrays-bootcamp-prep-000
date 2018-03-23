4fff5a4a14ab8336d9e8b406d51d5c0be098a92d
John Rodriguez <jcr2125@gmail.com>
2018-03-23 02:34:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr){
 }
 
 function removeElementFromBeginningOfArray(arr){
-  arr.slice(1);
-  return arr;
+  return arr.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(arr){
@@ -37,5 +36,7 @@ function destructivelyRemoveElementFromEndOfArray(arr){
   return arr;
 }
 
-function removeElementFromEndOfArray()
+function removeElementFromEndOfArray(arr){
+  return arr.slice(0, arr.length - 1);
+}
 
